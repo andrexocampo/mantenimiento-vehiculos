68ddb17e79f55697d7e4819970f15dc3116f8072
Andres <Andres@DESKTOP-R7O8ATR>
2025-10-11 13:03:10 -0500

    - Creación de los paquetes: model, interfaces,controller,service ,repository,interfaces, interfacesService - Creación de la clase Vehiculo, en el modelo

---
+++ src/main/java/com/portafolio/mantenimiento_vehiculos/model/Vehiculo.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portafolio.mantenimiento_vehiculos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Andres
+ */
+@Entity
+@Table(name="Vehiculos")
+public class Vehiculo {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private LocalDate pagoSoat;
+    private LocalDate pagoTecno;
+    private LocalDate cambioAceite;
+    private int tiempoCambioAceite;
+    
+    public Vehiculo(){};
+    
+    public void setid(int id){
+        this.id=id;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setDescripcion(String descripcion){
+        this.descripcion=descripcion;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    public void setPagoSoat(LocalDate pagoSoat){
+        this.pagoSoat=pagoSoat;
+    }
+    public LocalDate getPagoSoat(){
+        return pagoSoat;
+    }
+    
+    public void setPagoTecno(LocalDate pagoTecno){
+        this.pagoTecno=pagoTecno;
+    }
+    public LocalDate getPagoTecno(){
+        return pagoTecno;
+    }
+    
+    
+    public void setCambioAceite(LocalDate cambioAceite){
+        this.cambioAceite=cambioAceite;
+    }
+    public LocalDate getCambioAceite(){
+        return cambioAceite;
+    }
+    
+    
+}
